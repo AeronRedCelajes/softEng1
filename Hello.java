ef601e032941ae57d751eec59bf8673ec0d004df
AeronRedCelajes <142370807+AeronRedCelajes@users.noreply.github.com>
2023-08-16 14:07:17 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
   public static void main(String[] args){
     System.out.println("Hello, Software Engineering Course!");
-    System.out.println("My name is CELAJES, Aeron!");
+    System.out.println("My name is CELAJES, Aeron Red!");
   } 
 }
